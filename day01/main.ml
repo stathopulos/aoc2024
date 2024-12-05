7f544f32ec84eb072cd7837924698d439adaffa0
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2024-12-04 18:38:30 -0700

    Solve day01 part 1

--- day01/main.ml
+++ day01/main.ml
@@ -1,5 +1,21 @@
-let read_lines (path : string) : string =
-  In_channel.with_open_bin path In_channel.input_all
-
+let read_lines path = In_channel.with_open_bin path In_channel.input_all
 let lines = String.split_on_char '\n'
-let () = read_lines "input.txt" |> lines |> List.hd |> print_endline
+
+let pairs str =
+  let l = String.split_on_char ' ' str |> List.filter_map int_of_string_opt in
+  (List.hd l, List.nth l 1)
+
+let part_1 a b =
+  let sorted_left = List.sort ( - ) a in
+  let sorted_right = List.sort ( - ) b in
+  List.fold_left2 (fun acc a b -> acc + abs (a - b)) 0 sorted_left sorted_right
+
+let () =
+  let a, b =
+    read_lines "input.txt"
+    |> lines
+    |> List.filter (( <> ) "")
+    |> List.map pairs
+    |> List.split
+  in
+  Printf.printf "Part 1: %d\n" (part_1 a b)
